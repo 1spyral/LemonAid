dbde551c231fe62a6f49862ba9a62c6853856344
1spyral <83098914+1spyral@users.noreply.github.com>
2024-08-17 19:40:18 -0400

    make button

--- Frontend/src/components/AddInventoryButton.jsx
+++ Frontend/src/components/AddInventoryButton.jsx
@@ -7,8 +7,9 @@ const AddInventoryButton = () => {
     const navigate = useNavigate();
     return(
         <>
-        <button className="p-0 w-screen h-72 text-center" onClick={() => navigate('../scan-foods')}>
-            Add Inventory
+        <button className="flex flex-col justify-center items-center p-0 w-screen h-72 text-center" onClick={() => navigate('../scan-foods')}>
+            <div className="py-5">Add Item</div>
+            <img src="../src/assets/upload.webp" alt="upload" className="w-12 h-12 rounded-md object-cover"/>
         </button>
         </>
     );
