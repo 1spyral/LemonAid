217fae79feece99887b44676aa2ec8c679fa1e71
will2dai4 <william.3dai@gmail.com>
2024-08-17 11:30:13 -0400

    finished homepage ish
    YIPPPEEEEE

--- Frontend/src/components/AddInventoryButton.jsx
+++ Frontend/src/components/AddInventoryButton.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 
 const AddInventoryButton = () => {
     return(
-        <button className="p-0 text-2xl text-center">
-            Add Inventory Item
+        <>
+        <button className="p-0 w-screen h-72 text-center">
+            Add Inventory
         </button>
-    )
+        </>
+    );
 }
 
 export default AddInventoryButton;
