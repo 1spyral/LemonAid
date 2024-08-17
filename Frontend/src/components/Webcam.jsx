27944e20bb7ec17560f3d8692d044cbcbf4b3d92
will2dai4 <william.3dai@gmail.com>
2024-08-17 14:01:33 -0400

    created camera

---
+++ Frontend/src/components/Webcam.jsx
@@ -0,0 +1,32 @@
+import React, { useRef } from "react";
+import Webcam from "react-webcam";
+
+const WebcamComponent = () => {
+  const webcamRef = useRef(null);
+
+  const capturePhoto = () => {
+    const imageSrc = webcamRef.current.getScreenshot();
+    console.log(imageSrc); 
+  };
+
+  return (
+    <div className="items-center relative">
+      <Webcam
+        audio={false}
+        height={720}
+        width={1080}
+        screenshotFormat="image/jpeg"
+        className="mt-10 mb-5 transform scale-x-[-1] rounded-3xl"
+        ref={webcamRef}
+      />
+      <button
+        onClick={capturePhoto}
+        className="relative left-1/2 transform -translate-x-1/2 w-1/3 h-10 rounded-xl bg-raspberry"
+      >
+        Capture Photo
+      </button>
+    </div>
+  );
+};
+
+export default WebcamComponent;
---
+++ Frontend/src/pages/Camera.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import WebcamComponent from '../components/Webcam';
+
+function Camera() {
+    return(
+        <div className="flex justify-center">
+            <WebcamComponent />
+        </div>
+    );
+}
+
+export default Camera;
