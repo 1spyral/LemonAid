82ca0e535934c58b56d35ea125b36f7e1106eb7f
1spyral <83098914+1spyral@users.noreply.github.com>
2024-08-17 02:39:55 -0400

    integrate openai vision

--- Backend/const.py
+++ Backend/const.py
@@ -1,2 +1,18 @@
 VALID_FILE_TYPES = {'png', 'jpg', 'jpeg'}
 PHOTO_PATH = "imgs/"
+PROMPT = """The current date is August 17, 2024.
+            
+            Please output the response to these questions in a dictionary object with the following format:
+            {
+            isfooditem: yes or no
+            name: string
+            confidence: integer
+            guessnumberofdays: integer
+            isdateonimage: yes or no
+            dateonimage: date (YYYY-MM-DD) or none
+            }
+
+            Is there a food item in this image, and if so, what is it?
+            How confident is this prediction of the food item? (as a percentage)
+            In about how many days will this product reach its best before or expiry date?
+            Is there a best before or expiry date listed on the image? If so, what is it?"""
