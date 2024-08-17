aef5c712bc512ad945d53a320480c8e1e73b913c
will2dai4 <william.3dai@gmail.com>
2024-08-17 17:00:33 -0400

    added manual submission

--- Frontend/src/components/Webcam.jsx
+++ Frontend/src/components/Webcam.jsx
@@ -10,11 +10,17 @@ const WebcamComponent = ({ onCapture }) => {
         onCapture(imageSrc);
     }
     console.log(imageSrc);
+
+    const foodName = document.getElementById("food-name-input").value;
+    const foodExpiry = document.getElementById("food-expiry-input").value;
+
     const data = new FormData();
 
     data.append("image", imageSrc);
-    data.append("name", "");
-    data.append("expiry", "");
+    data.append("name", foodName);
+    data.append("expiry", foodExpiry);
+    console.log(foodName, foodExpiry);
+    
     fetch('http://127.0.0.1:5000/api/upload_item', {
         method: "POST",
         body: data,
@@ -33,6 +39,16 @@ const WebcamComponent = ({ onCapture }) => {
         className="mt-2 mb-5 transform scale-x-[-1] rounded-3xl"
         ref={webcamRef}
       />
+      <form id='food-info-form' className='gap-3'>
+        <div>
+            <label className='block'>Food Name</label>
+            <input type="text" id="food-name-input" className="border-2"/>
+        </div>
+        <div>
+            <label className='block'>Expiry Date (yyyy-mm-dd)</label>
+            <input type="text" id="food-expiry-input" className="border-2"/>
+        </div>
+      </form>
       <button
         onClick={capturePhoto}
         className="relative left-1/2 transform -translate-x-1/2 w-1/3 h-10 rounded-xl bg-raspberry"
