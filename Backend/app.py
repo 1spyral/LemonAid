82b91d15fbd41e24ff458a8e5f9f51ebf8c05a54
1spyral <83098914+1spyral@users.noreply.github.com>
2024-08-16 23:38:06 -0400

    update backend

--- Backend/app.py
+++ Backend/app.py
@@ -14,3 +14,26 @@ server = Server()
 app.run()
 
 
+@app.route("/api/upload_item", methods=["POST"])
+def upload_item():
+    """
+    Upload food item image or text description to the server.
+
+    Request:
+    {
+        "image": png, jpg or jpeg image,
+        "description": "2 liter vanilla ice cream bucket" (optional)
+        "expiry": "2023-12-31" (optional)
+    }
+    Response:
+    {
+        "id": "123456",
+        "name": "vanilla ice cream",
+        "recommended_location": "freezer",
+        "expiry": "2023-12-31",
+        "status": "success"
+    }
+    """
+    
+
+
