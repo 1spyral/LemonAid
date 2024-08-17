f14ce0c19489c28aa8965248fd607e6a7c868ba7
will2dai4 <william.3dai@gmail.com>
2024-08-17 00:04:16 -0400

    something

---
+++ Frontend/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import { Component } from 'react';
+
+function Home() {
+    return (
+        <div className="text-gray-200">
+            <h1 className="text-green-600">Food Thingy Website</h1>
+        </div>
+    );
+}
+
+export default Home;
