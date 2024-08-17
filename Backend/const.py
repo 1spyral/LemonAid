4cfef73cedf32b12e1192eb035c11872f3c365ba
William Wu <williamwu277@gmail.com>
2024-08-17 02:58:46 -0400

    Scanner File
    Implemented scanner file

--- Backend/const.py
+++ Backend/const.py
@@ -1,2 +1,19 @@
 VALID_FILE_TYPES = {'png', 'jpg', 'jpeg'}
 PHOTO_PATH = "imgs/"
+GPT_MODEL = "gpt-4o"
+GPT_MAX_TOKENS = 300
+GPT_PROMPT = """The current date is August 17, 2024.
+            
+                            Please output the response to these questions in a dictionary object with the following format:
+                            {
+                                name: string
+                                confidence: integer
+                                guessnumberofdays: integer
+                                isdateonimage: yes or no
+                                dateonimage: date (YYYY-MM-DD) or none
+                            }
+
+                            What food item is in this image?
+                            How confident is this prediction of the food item? (as a percentage)
+                            In about how many days will this product reach its best before or expiry date?
+                            Is there a best before or expiry date listed on the image? If so, what is it?"""
--- Backend/scanner.py
+++ Backend/scanner.py
@@ -1,5 +1,64 @@
+from openai import OpenAI
+from secret import API_KEY, ORG_ID
+from base64 import b64encode
+from const import GPT_MODEL, GPT_PROMPT, GPT_MAX_TOKENS
+
+'''
+
+function to convert a local image path to a base64 string
+
+'''
+def b64_encode_file(path):
+
+    with open(path, "rb") as file:
+        encoding = b64encode(file.read()).decode('utf-8')
+        return encoding
+
+
+''' 
+
+class to return ChatGPT predictions about the image
+
+Example usage: 
+
+image = "image.png"
+b64_image = b64_encode_file(image)
+r = Scanner()
+print(r.scan(b64_image))
+
+'''
 class Scanner:
-    """
-    Scanner class for identifying the image
-    """
+
+    def __init__(self):
+
+        self.client = OpenAI(
+            api_key=API_KEY,
+            organization=ORG_ID
+        )
+
+
+    def scan(self, b64_encoded_file):
+
+        response = self.client.chat.completions.create(
+            model=GPT_MODEL,
+            messages=[
+                {
+                    "role": "user",
+                    "content": [
+                        {
+                            "type": "text", "text": GPT_PROMPT
+                        },
+                        {
+                            "type": "image_url",
+                            "image_url": {
+                                "url": f"data:image/jpeg;base64,{b64_encoded_file}",
+                            },
+                        },
+                    ],
+                }
+            ],
+            max_tokens=GPT_MAX_TOKENS,
+        )
+
+        return response.choices[0]
 
